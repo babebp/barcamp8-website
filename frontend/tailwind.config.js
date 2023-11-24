07f02227a35eb82e6d0f6f3c5c67acad2d48a361
babebp <tpacharawut@hotmail.com>
2023-11-24 15:37:08 +0700

    merge

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: '#504128',
+        cream: '#fdf6ea'
+      }
+    },
   },
   plugins: [],
 }
-

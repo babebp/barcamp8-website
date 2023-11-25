fb49b765c7ac0deb6ed96b05ebc9baef3c24fdd4
Nicha Vikromrotjananan <nnbaitei@gmail.com>
2023-11-25 23:58:16 +0700

    timeline

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         brown: '#504128',
         cream: '#fdf6ea',
-        yellow: '#FF8C00'
+        yellow: '#FF8C00',
+        white: '#FAF6F6',
+
       }
     },
   },

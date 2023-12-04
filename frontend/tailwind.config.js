d40f3d385a07d76b3bb713f0685b8babfb14c2ca
Nicha Vikromrotjananan <nnbaitei@gmail.com>
2023-12-04 21:49:49 +0700

    add subtitle event in homepage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
         yellow: '#FF8C00',
         white: '#FAF6F6',
 
-      }
+      },
+      animation: {
+        marquee: 'marquee 21s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+
     },
   },
   plugins: [],
